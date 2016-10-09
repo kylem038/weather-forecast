5643217c9c5151cc97d417376c9ec9a6b64d852e
Casey Cross <hellocaseycross@gmail.com>
2016-10-09 17:30:44 -0600

    Three passing tests

--- src/components/FiveDay.test.js
+++ src/components/FiveDay.test.js
@@ -11,4 +11,22 @@ describe('FiveDay Forecast', () => {
 
     expect(wrapper.is('.fiveDayForecast')).toEqual(true);
   });
+
+  it('should have a temp element if a temp is passed as a prop', () => {
+    const wrapper = shallow(<FiveDay temp={'85'} />);
+
+    expect(wrapper.find('.Temp').length).toEqual(1);
+  });
+
+  it('should not have a temp element if a quantity is not passed as a prop', () => {
+    const wrapper = shallow(<FiveDay temp={undefined}/>);
+
+    expect(wrapper.find('.Temp').length).toEqual(0);
+  });
+
+  it('should display the correct temp if temp is passed in', () => {
+    const wrapper = shallow(<FiveDay />);
+
+    expect(wrapper.find('.Temp').text()).toEqual('Temp: 289.5');
+  });
 });
