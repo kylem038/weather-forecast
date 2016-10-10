a9363b865e04500a26950e26ef7f10b7e21ff7b7
Casey Cross <hellocaseycross@gmail.com>
2016-10-09 22:02:56 -0600

    Add mock store test

---
+++ src/containers/Containers.test.js
@@ -0,0 +1,38 @@
+import { Provider } from 'react-redux';
+import React from 'react';
+import { mount } from 'enzyme';
+import HomePage from '../components/HomePage';
+
+// create a mock store
+export const mockStore = (state) => {
+	return {
+		default: () => {},
+		subscribe: () => {},
+		dispatch: () => {},
+		getState: () => {
+			return { ...state };
+		},
+	};
+};
+
+describe('Main Container', () => {
+  function setup() {
+    const store = mockStore({});
+    const wrapper = mount(
+      <Provider store={store}>
+        <HomePage />
+      </Provider>
+    );
+
+    const Component = wrapper.find(HomePage);
+    return {
+      Component
+    };
+  }
+
+    it('should render Main container', () => {
+      const { Component } = setup();
+
+      expect(Component.length).toBeTruthy();
+    });
+  });
