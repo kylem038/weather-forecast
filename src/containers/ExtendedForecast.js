9c8ec67f486ea7f992775845408bcfdf2ad6f2ea
Mike Sausa <mikejsausa@gmail.com>
2016-10-10 18:25:13 -0600

    data issues

--- src/containers/ExtendedForecast.js
+++ src/containers/ExtendedForecast.js
@@ -3,8 +3,9 @@ import { currentCity, fetchForecast } from '../actions/actions';
 import ExtendedForecast from '../components/ExtendedForecast';
 
 const mapStateToProps = state => {
-  if (!state.ExtendedLocalCityReducer.main.weatherdata) return {};
+  if (!state.ExtendedLocalCityReducer.city.id) return {};
   const data = state.ExtendedLocalCityReducer.city.id;
+  console.log(data);
   return {
     data
   };
